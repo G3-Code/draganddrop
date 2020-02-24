8a831447e27c6773c533cfea06925c5dd2120b0d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-24 08:58:29 -0600

    Tried adding styled-components but it has to be installed first

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDropzone } from "react-dropzone";
 
+function Basic(props) {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  // const Container = styled.div`
+  //   flex: 1;
+  //   display: flex;
+  //   flex-direction: column;
+  //   align-items: center;
+  //   padding: 20px;
+  //   border-width: 2px;
+  //   border-radius: 2px;
+  //   border-color: ${props => getColor(props)};
+  //   border-style: dashed;
+  //   background-color: #fafafa;
+  //   color: #bdbdbd;
+  //   outline: none;
+  //   transition: border 0.24s ease-in-out;
+  // `;
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' Drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Basic />
     </div>
   );
 }
