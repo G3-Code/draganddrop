79e1db018989efc0fa3d357f366189df59ba3a0c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-25 04:56:06 -0600

    working with styled components

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,48 +1,18 @@
 import React from "react";
-import { useDropzone } from "react-dropzone";
+// import { Button } from "./components/Button";
+import styled from "styled-components";
 
-function Basic(props) {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+const Button = styled.button`
+background:blue,
+border-radius:3px,
+border:  1px solid red,
+padding: 20px`;
 
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
-  // const Container = styled.div`
-  //   flex: 1;
-  //   display: flex;
-  //   flex-direction: column;
-  //   align-items: center;
-  //   padding: 20px;
-  //   border-width: 2px;
-  //   border-radius: 2px;
-  //   border-color: ${props => getColor(props)};
-  //   border-style: dashed;
-  //   background-color: #fafafa;
-  //   color: #bdbdbd;
-  //   outline: none;
-  //   transition: border 0.24s ease-in-out;
-  // `;
-
-  return (
-    <section className="container">
-      <div {...getRootProps({ className: "dropzone" })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' Drop some files here, or click to select files</p>
-      </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </section>
-  );
-}
 function App() {
   return (
     <div>
-      <Basic />
+      Render components to test here!!
+      <Button />
     </div>
   );
 }
