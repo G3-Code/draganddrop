7c8d19ce656d06083771af15f99c1bf7274cbb67
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-25 05:59:43 -0600

    Working with styled coponents

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,27 +1,44 @@
 import React from "react";
 // import { Button } from "./components/Button";
-import styled from "styled-components";
+import styled, { ThemeProvider, css } from "styled-components";
 
+const theme = {
+  primary: "red",
+  secondary: "blue",
+  alert: "orange",
+  font: "sans-serif"
+};
 const Button = styled.button`
   background: ${props => (props.primary ? "red" : "green")};
   border-radius: 3px;
   border: 1px solid red;
   padding: 7px 10px;
   color: #fff;
+  ${props =>
+    props.color &&
+    css`
+      background: ${props => props.theme[props.color]};
+    `}
   &:hover {
     color: blue;
   }
 `;
 
+const H1 = styled.h1`
+  font-family: ${props => props.theme.font};
+`;
+
 function App() {
   return (
-    <div>
-      Render components to test here!!
+    <ThemeProvider theme={theme}>
       <div>
-        <button>Create</button>
-        <Button>Create </Button>
+        <H1>Render components to test here!!</H1>
+        <div>
+          <button>Create</button>
+          <Button color="alert">Create </Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
