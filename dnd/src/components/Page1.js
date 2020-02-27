fe23a5e9c4fc28e22f87efb021bad929dee9db6e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-27 05:43:02 -0600

    Learing a11y

--- dnd/src/components/Page1.js
+++ dnd/src/components/Page1.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "../App.css";
 
 export default function Page1() {
-  return <div>Page 1</div>;
+  return (
+    <div>
+      <div>Visually Hidden Beginning</div>
+      <button className="visually-hidden">Demo Buttons</button>
+      <div>Visually Hidden Ending</div>
+    </div>
+  );
 }
