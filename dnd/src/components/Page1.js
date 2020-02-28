55ac2bcece918f866564f7751811b640efbf889b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-28 05:44:37 -0600

    Added fragment

--- dnd/src/components/Page1.js
+++ dnd/src/components/Page1.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "../App.css";
 
 export default function Page1() {
   return (
-    <div>
-      <div>Visually Hidden Beginning</div>
-      <button className="visually-hidden">Demo Buttons</button>
-      <div>Visually Hidden Ending</div>
-    </div>
+    <Fragment>
+      <div>
+        <div>Visually Hidden Beginning</div>
+        <button className="visually-hidden">Demo Buttons</button>
+        <div>Visually Hidden Ending</div>
+      </div>
+    </Fragment>
   );
 }
