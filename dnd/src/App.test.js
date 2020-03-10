0ca207f6eda98f4bf5915c51291c0f1d1595218e
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-03-10 05:01:47 -0500

    installing for redux

--- dnd/src/App.test.js
+++ dnd/src/App.test.js
@@ -5,6 +5,7 @@ import { mount } from "enzyme";
 describe("Testing href", () => {
   it("Testing href", () => {
     const wrapper = mount(<App />);
-    console.log(wrapper.debug());
+    console.log(wrapper.find("div").getElement());
+    //expect(wrapper.find("div")).toBeOfLength(1);
   });
 });
--- dnd/src/index.js
+++ dnd/src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import thunk from "react-redux";
 
 ReactDOM.render(<App />, document.getElementById("root"));
